10b0aeb6efcd99caf68e9731c602f1c7be87f6d9
Arman <armanasadi121@gmail.com>
2025-02-06 23:49:50 +0330

    Added game-versions and loaders option for resource install + enhanced api check

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -7,29 +7,34 @@ import {downloadAndSaveFromURL} from "../utils/downloader";
 export async function InstallCommand(
     resourceType: string,
     resources: string[],
-    api: string,
-
+    game_versions?: string[],
+    loaders?: string[],
+    api?: string,
 ) {
-    switch (resourceType) {
-        case 'mod': {
-            for (const resource of resources) {
-                try {
-                    const project: Project = await modrinthClient.getProject(resource)
-                    const file =
-                        Object.assign(new ProjectVersion, (await modrinthClient.getProjectVersions(
-                            {
-                                project: project,
-                            }
-                        ))[0]).getPrimaryFile();
-                    if (file?.url) {
-                        generateDirectory(`${process.cwd()}/mods/`)
-                        await downloadAndSaveFromURL(file.url, `${process.cwd()}/mods/${file.filename}`);
-                    }
-                    console.log();
-                } catch (error) {
-                    console.log(error)
+    if (api === 'modrinth') {
+        for (const resource of resources) {
+            try {
+                const project: Project = await modrinthClient.getProject(resource)
+                const param = {
+                    project: project,
+                    game_versions: game_versions?.toString(),
+                    loaders: loaders?.toString(),
                 }
+                const file =
+                    Object.assign(new ProjectVersion, (await modrinthClient.getProjectVersions(param))[0]).getPrimaryFile();
+                if (file?.url) {
+                    const resourceFolder =
+                        resourceType === 'mod' ? 'mods' :
+                        resourceType === 'plugin' ? 'plugins' :
+                        resourceType === 'resourcepack' ? 'resourcepacks' :
+                        resourceType === 'shaderpack' ? 'shaderpacks' : 'mcpm';
+                    generateDirectory(`${process.cwd()}/${resourceFolder}/`)
+                    await downloadAndSaveFromURL(file.url, `${process.cwd()}/${resourceFolder}/${file.filename}`);
+                }
+            } catch (error) {
+                console.log(error)
             }
         }
     }
+
 }
