c85903c9989ae149550996dd2d9108bd08d8c16d
Arman <armanasadi121@gmail.com>
2025-02-06 23:46:45 +0330

    Added game-versions and loaders option for install command

--- src/commands.ts
+++ src/commands.ts
@@ -11,17 +11,21 @@ export function registerCommands(program: Command) {
             )
         )
         .argument('<RESOURCES...>', 'name or ID of Resource.')
+        .option('--game-versions -g [game_versions...]', 'Minecraft versions for resource install.')
+        .addOption(new Option('--loaders -l [loaders...]', 'Minecraft loaders for resource install.')
+            .choices(['forge', 'fabric', 'spigot', 'bukkit', 'paper', 'quilt', 'neoforge', 'bungeecord', 'velocity', 'sponge', 'folia', 'purpur'])
+        )
         .addOption(new Option('--api', `defines API for getting resources.`)
             .choices(['modrinth'])
             .default('modrinth')
         )
-        .action((resourceType, resources, api) =>
-            InstallCommand(resourceType, resources, api)
+        .action((resourceType, resources, options) =>
+            InstallCommand(resourceType, resources, options.gameVersions, options.loaders, options.api)
         );
 
     program.command('init')
         .description('Initializes mcpm for directory within concrete.json')
-        .addOption(new Option('--game-version [game_version]', 'Minecraft\'s version of directory. this is based on what version your instance/server is using.'))
+        .addOption(new Option('--game-version [game_version]', 'Minecraft version of directory. this is based on what version your instance/server is using.'))
         .addOption(new Option('--loader [loader]', 'Minecraft Loader of directory (forge, fabric, paper, etc...)'))
         .action((options) =>
             InitCommand(options.gameVersion, options.loader)
