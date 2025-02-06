bf2bdf57d71e64059785ac22c2c24724821948ef
Arman <armanasadi121@gmail.com>
2025-02-06 23:50:09 +0330

    Added game-versions and loaders option for resource install

--- src/apis/modrinth/routes/versions/getProjectVersions.ts
+++ src/apis/modrinth/routes/versions/getProjectVersions.ts
@@ -7,11 +7,25 @@ import {modrinthClient} from "../../../../main";
 // }
 export interface ProjectVersionOptions {
     project: Project;
-    loaders?: string[];
-    game_versions?: string[];
+    loaders?: string[] | string;
+    game_versions?: string[] | string;
     featured?: boolean;
 }
 
 export async function getProjectVersions(options: ProjectVersionOptions): Promise<ProjectVersion[]> {
-    return await modrinthClient._request<ProjectVersion[]>("GET", `project/${options.project.id}/version`, {});
+    const params = new URLSearchParams()
+
+    if (options.loaders) {
+        options.loaders = Array.isArray(options.loaders) ? `["${options.loaders.join('","')}"]` : `["${options.loaders}"]`;
+        options.loaders && params.set("loaders", options.loaders);
+    }
+    if (options.game_versions) {
+        options.game_versions = Array.isArray(options.game_versions) ? `["${options.game_versions.join('","')}"]` : `["${options.game_versions}"]`;
+        options.game_versions && params.set("game_versions", options.game_versions);
+    }
+    options.featured && params.set("featured", options.toString());
+
+    return await modrinthClient._request<ProjectVersion[]>("GET", `project/${options.project.id}/version`, {
+        query: params
+    });
 }
