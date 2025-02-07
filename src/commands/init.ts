6e3d980c7f57b733de7c24bdc011189d95126ab5
Arman <armanasadi121@gmail.com>
2025-02-07 19:54:38 +0330

    Fixed readLine not working

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,29 +1,26 @@
 import concreteDefault from '../resources/concrete-default.json';
-import * as readLine from "readline";
+import * as readLine from "readline/promises";
 import 'colorts/lib/string'
 import {writeFileSync} from "node:fs";
-import {ConcreteConfig} from "../concrete";
+import {concreteConfig} from "../main";
 
 export async function InitCommand(game_version: string | undefined, loader: string | undefined) {
     let concreteJson = concreteDefault;
     let rl = readLine.createInterface(process.stdin, process.stdout);
 
-    if (new ConcreteConfig().isExists) {
-        console.error('mcpm is already Initialized on current directory (concrete.json)'.red)
-        return rl.close();
-    }
+    // concreteConfig.checkForConcreteFile(false)
 
     if (game_version) {
         concreteJson.game_version = game_version;
     } else {
-        rl.question('Enter Minecraft version: ', (answer) => concreteJson.game_version = answer);
+        concreteJson.game_version = await rl.question('Enter Minecraft version: ');
     }
     console.log('Set Minecraft version: '.green + `${concreteJson.game_version}`.yellow)
 
     if (loader) {
         concreteJson.loader = loader;
     } else {
-        rl.question('Enter Minecraft loader: ', (answer) => concreteJson.loader = answer);
+        concreteJson.loader = await rl.question('Enter Minecraft loader: ');
     }
     console.log('Set Minecraft loader: '.green + `${concreteJson.loader}`.yellow)
 
