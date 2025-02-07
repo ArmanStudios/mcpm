e335cf75d687aaf2ddddce69193916484eff7c71
Arman <armanasadi121@gmail.com>
2025-02-07 20:53:01 +0330

    Changed readline package to readline-sync

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,32 +1,29 @@
 import concreteDefault from '../resources/concrete-default.json';
-import * as readLine from "readline/promises";
+import * as rl from "readline-sync";
 import 'colorts/lib/string'
 import {writeFileSync} from "node:fs";
 import {concreteConfig} from "../main";
 
 export async function InitCommand(game_version: string | undefined, loader: string | undefined) {
     let concreteJson = concreteDefault;
-    let rl = readLine.createInterface(process.stdin, process.stdout);
 
     concreteConfig.checkForConcreteFile(false)
 
     if (game_version) {
         concreteJson.game_version = game_version;
     } else {
-        concreteJson.game_version = await rl.question('Enter Minecraft version: ');
+        concreteJson.game_version = rl.question('Enter Minecraft version: ');
     }
     console.log('Set Minecraft version: '.green + `${concreteJson.game_version}`.yellow)
 
     if (loader) {
         concreteJson.loader = loader;
     } else {
-        concreteJson.loader = await rl.question('Enter Minecraft loader: ');
+        concreteJson.loader = rl.question('Enter Minecraft loader: ');
     }
     console.log('Set Minecraft loader: '.green + `${concreteJson.loader}`.yellow)
 
     writeFileSync(process.cwd()+'/concrete.json', JSON.stringify(concreteJson, null, 2));
 
     console.log('Successfully Initialized mcpm for directory:'.green + ' concrete.json'.yellow);
-
-    rl.close()
 }
