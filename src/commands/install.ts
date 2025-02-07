f2eb20b4108f90916847e74270185492b3cc15d3
Arman <armanasadi121@gmail.com>
2025-02-07 19:54:14 +0330

    Concrete Overhaul: ConcreteConfig setup for install

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -1,8 +1,9 @@
 import {Project} from "../apis/modrinth/types/Project"
-import {modrinthClient} from "../main";
+import {concreteConfig, modrinthClient} from "../main";
 import {ProjectVersion} from "../apis/modrinth/types/ProjectVersion";
 import {generateDirectory} from "../utils/directoryManager";
 import {downloadAndSaveFromURL} from "../utils/downloader";
+import {ConcreteFileNotFoundException} from "../exceptions/concreteExceptions";
 
 export async function InstallCommand(
     resourceType: string,
@@ -20,8 +21,18 @@ export async function InstallCommand(
                     game_versions: game_versions?.toString(),
                     loaders: loaders?.toString(),
                 }
-                const file =
-                    Object.assign(new ProjectVersion, (await modrinthClient.getProjectVersions(param))[0]).getPrimaryFile();
+                const version = Object.assign(new ProjectVersion, (await modrinthClient.getProjectVersions(param))[0])
+
+                if (version.id) {
+                    if (concreteConfig.checkForResourceVersion(version.id)) {
+                        console.log(
+                            `Resource ${project.title?.bold} with the same version of ${version.version_number?.bold} is already installed.`.yellow
+                        )
+                        return;
+                    }
+                }
+
+                const file = version.getPrimaryFile();
                 if (file?.url) {
                     const resourceFolder =
                         resourceType === 'mod' ? 'mods' :
@@ -29,10 +40,21 @@ export async function InstallCommand(
                         resourceType === 'resourcepack' ? 'resourcepacks' :
                         resourceType === 'shaderpack' ? 'shaderpacks' : 'mcpm';
                     generateDirectory(`${process.cwd()}/${resourceFolder}/`)
-                    await downloadAndSaveFromURL(file.url, `${process.cwd()}/${resourceFolder}/${file.filename}`);
+                    await downloadAndSaveFromURL(
+                        file.url,
+                        `${process.cwd()}/${resourceFolder}/${file.filename}`,
+                        { customName: `${project.title} (${version.version_number} - ${file.filename})` }
+                    )
                 }
-            } catch (error) {
-                console.log(error)
+                if (concreteConfig.checkForResource(project)) {
+                    concreteConfig.removeResource(project)
+                }
+                concreteConfig.addResource(project, version, file?.filename)
+            } catch (e) {
+                if (e instanceof ConcreteFileNotFoundException) {
+                    console.error(e.message.red);
+                }
+                console.error(e)
             }
         }
     }
